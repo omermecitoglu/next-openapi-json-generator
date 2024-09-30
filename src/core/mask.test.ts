030f68a13e4e6e05ef2f7a4d1057703659e0cdd1
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-09-30 12:11:34 +0700

    test: add tests for maskWithReference

---
+++ src/core/mask.test.ts
@@ -0,0 +1,115 @@
+import { beforeEach, describe, expect, it, jest } from "@jest/globals";
+import z, { type ZodType } from "zod";
+import maskWithReference from "./mask";
+import type { SchemaObject } from "@omer-x/openapi-types/schema";
+
+
+describe("maskWithReference", () => {
+  const storedSchemas: Record<string, ZodType> = {
+    User: z.object({
+      id: z.string(),
+      name: z.string(),
+    }),
+  };
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should return a reference if schema matches a stored schema", () => {
+    const schema: SchemaObject = {
+      type: "object",
+      properties: {
+        id: { type: "string" },
+        name: { type: "string" },
+      },
+    };
+
+    const result = maskWithReference(schema, storedSchemas, true);
+    expect(result).toEqual({ $ref: "#/components/schemas/User" });
+  });
+
+  it("should return the schema if it contains a $ref", () => {
+    const schema: SchemaObject = {
+      $ref: "#/components/schemas/SomeSchema",
+    };
+    const result = maskWithReference(schema, storedSchemas, true);
+    expect(result).toEqual(schema);
+  });
+
+  it("should process oneOf schemas", () => {
+    const schema = {
+      oneOf: [
+        { type: "string" },
+        { type: "number" },
+      ],
+    } as unknown as SchemaObject;
+    const result = maskWithReference(schema, storedSchemas, true);
+    expect(result).toEqual({
+      oneOf: [
+        { type: "string" },
+        { type: "number" },
+      ],
+    });
+  });
+
+  it("should process anyOf schemas", () => {
+    const schema = {
+      anyOf: [
+        { type: "string" },
+        { type: "number" },
+      ],
+    } as unknown as SchemaObject;
+    const result = maskWithReference(schema, storedSchemas, true);
+    expect(result).toEqual({
+      anyOf: [
+        { type: "string" },
+        { type: "number" },
+      ],
+    });
+  });
+
+  it("should process object schemas", () => {
+    const schema: SchemaObject = {
+      type: "object",
+      properties: {
+        id: { type: "string" },
+        name: { type: "string" },
+      },
+    };
+    const result = maskWithReference(schema, storedSchemas, true);
+    expect(result).toEqual({
+      $ref: "#/components/schemas/User",
+    });
+  });
+
+  it("should process array schemas", () => {
+    const schema: SchemaObject = {
+      type: "array",
+      items: { type: "string" },
+    };
+    const result = maskWithReference(schema, storedSchemas, true);
+    expect(result).toEqual({
+      type: "array",
+      items: { type: "string" },
+    });
+  });
+
+  it("should process array schemas with multiple items", () => {
+    const schema: SchemaObject = {
+      type: "array",
+      items: [
+        { type: "string" },
+        { type: "number" },
+      ],
+    };
+    const result = maskWithReference(schema, storedSchemas, true);
+    expect(result).toEqual({
+      type: "array",
+      items: [
+        { type: "string" },
+        { type: "number" },
+      ],
+    });
+  });
+});
