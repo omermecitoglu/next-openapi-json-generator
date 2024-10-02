c168f6358551c9174767a1425e05a814b9b787ea
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-10-02 17:28:57 +0700

    test: update example code in getRouteExports test

--- src/core/next.test.ts
+++ src/core/next.test.ts
@@ -29,7 +29,7 @@ describe("findAppFolderPath", () => {
   });
 });
 
-describe("getRouteExports", () => {
+describe("getRouteExports", async () => {
   const mockSchemas = {
     UserDTO: z.object({
       id: z.string(),
@@ -42,53 +42,15 @@ describe("getRouteExports", () => {
   };
   const mockRoutePath = "/mock/path";
   const mockRouteDefinerName = "defineRoute";
-  const rawCode = `
-import ${mockRouteDefinerName} from "@omer-x/next-openapi-route-handler";
-import z from "zod";
-import db from "~/database";
-import { NewUserDTO, UserDTO } from "~/models/user";
-import createUser from "~/operations/createUser";
-import getUsers from "~/operations/getUsers";
-
-export const { GET } = ${mockRouteDefinerName}({
-  operationId: "getUsers",
-  method: "GET",
-  summary: "Get all users",
-  description: "Retrieve a list of users",
-  tags: ["Users"],
-  queryParams: z.object({
-    select: UserDTO.keyof().array().default([])
-      .describe("List of the column names"),
-  }),
-  action: async ({ queryParams }) => {
-    return Response.json(await getUsers(db, queryParams.select));
-  },
-  responses: {
-    200: { description: "Returns a list of users", content: UserDTO, isArray: true },
-  },
-});
-
-export const { POST } = ${mockRouteDefinerName}({
-  operationId: "createUser",
-  method: "POST",
-  summary: "Create user",
-  description: "Create a new user",
-  tags: ["Users"],
-  requestBody: NewUserDTO,
-  action: async ({ body }) => {
-    const user = await createUser(db, body);
-    return Response.json(user, { status: 201 });
-  },
-  responses: {
-    201: { description: "User created successfully", content: UserDTO },
-    409: { description: "Email already exists" },
-  },
-});
-  `;
+  const repoName = "omermecitoglu/example-user-service";
+  const branchName = "main";
+  const filePath = "src/app/users/route.ts";
+  const response = await fetch(`https://raw.githubusercontent.com/${repoName}/refs/heads/${branchName}/${filePath}`);
+  const example = await response.text();
 
   beforeEach(() => {
     jest.clearAllMocks();
-    (fs.readFile as jest.Mock<typeof fs.readFile>).mockResolvedValue(rawCode);
+    (fs.readFile as jest.Mock<typeof fs.readFile>).mockResolvedValue(example);
   });
 
   it("should read the file content", async () => {
