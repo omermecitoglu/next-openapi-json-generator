9e2dce946fc98911f4921188842df4fdd8a7d6cb
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-12-12 18:25:32 +0700

    fix: imported queryParams bug

--- src/core/injectSchemas.ts
+++ src/core/injectSchemas.ts
@@ -5,7 +5,8 @@ export default function injectSchemas(code: string, refName: string) {
 
   const preservedCodeWithSchemasInjected = preservedCode
     .replace(new RegExp(`\\b${refName}\\.`, "g"), `global.schemas[${refName}].`)
-    .replace(new RegExp(`\\b${refName}\\b`, "g"), `"${refName}"`);
+    .replace(new RegExp(`\\b${refName}\\b`, "g"), `"${refName}"`)
+    .replace(new RegExp(`queryParams:\\s*['"\`]${refName}['"\`]`, "g"), `queryParams: global.schemas["${refName}"]`);
 
   return restoreStrings(preservedCodeWithSchemasInjected, replacements);
 }
