5a8547acd2a2915ce0b202c828320d596e09001c
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-12-13 10:51:56 +0700

    test: bundlePaths with predefined schemas

--- src/core/route.test.ts
+++ src/core/route.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "@jest/globals";
+import { type ZodType, z } from "zod";
 import { bundlePaths, createRouteRecord } from "./route";
 import type { OperationObject } from "@omer-x/openapi-types/operation";
-import type { ZodType } from "zod";
 
 describe("createRouteRecord", () => {
   it("should create a route record with the correct method, path, and apiData", () => {
@@ -99,6 +99,131 @@ describe("bundlePaths", () => {
           requestBody: undefined,
           responses: undefined,
           summary: "Create user",
+        },
+      },
+    });
+  });
+
+  it("should create references for the responses that are identical to pre-defined schemas", () => {
+    const source = [
+      {
+        method: "get",
+        path: "/user",
+        apiData: {
+          summary: "Get user",
+          responses: {
+            200: {
+              description: "Successful response",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
+                    properties: {
+                      name: {
+                        type: "string",
+                      },
+                      email: {
+                        type: "string",
+                        format: "email",
+                      },
+                    },
+                    required: ["name", "email"],
+                    additionalProperties: false,
+                  },
+                },
+              },
+            },
+          },
+        } as OperationObject,
+      },
+    ];
+    const storedSchemas: Record<string, ZodType> = {
+      UserDTO: z.object({
+        name: z.string(),
+        email: z.string().email(),
+      }),
+    };
+
+    const result = bundlePaths(source, storedSchemas);
+
+    expect(result).toStrictEqual({
+      "/user": {
+        get: {
+          parameters: undefined,
+          requestBody: undefined,
+          responses: {
+            200: {
+              content: {
+                "application/json": {
+                  schema: {
+                    $ref: "#/components/schemas/UserDTO",
+                  },
+                },
+              },
+              description: "Successful response",
+            },
+          },
+          summary: "Get user",
+        },
+      },
+    });
+  });
+
+  it("should handle the cases where schema is undefined or referenced already", () => {
+    const source = [
+      {
+        method: "get",
+        path: "/user",
+        apiData: {
+          summary: "Get user",
+          parameters: [
+            {
+              $ref: "#/components/parameters/QueryLimit",
+            },
+          ],
+          responses: {
+            200: {
+              description: "Successful response",
+              content: {
+                "application/json": {
+                  schema: undefined,
+                },
+              },
+            },
+            404: {
+              $ref: "#/components/responses/NotFound",
+            },
+          },
+        } as OperationObject,
+      },
+    ];
+    const storedSchemas: Record<string, ZodType> = {};
+
+    const result = bundlePaths(source, storedSchemas);
+
+    expect(result).toStrictEqual({
+      "/user": {
+        get: {
+          parameters: [
+            {
+              $ref: "#/components/parameters/QueryLimit",
+            },
+          ],
+          requestBody: undefined,
+          responses: {
+            200: {
+              content: {
+                "application/json": {
+                  schema: undefined,
+                },
+              },
+              description: "Successful response",
+            },
+            404: {
+              $ref: "#/components/responses/NotFound",
+            },
+          },
+          summary: "Get user",
         },
       },
     });
