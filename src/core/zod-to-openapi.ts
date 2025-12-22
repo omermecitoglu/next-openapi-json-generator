5cf1cef4c41096e9f2ec5a60326fad0049cfff63
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-12-22 12:47:36 +0700

    fix: missing schema descriptions

--- src/core/zod-to-openapi.ts
+++ src/core/zod-to-openapi.ts
@@ -1,25 +1,32 @@
 import { type ZodObject, type ZodType, z } from "zod";
 import type { SchemaObject } from "@omer-x/json-schema-types";
 
+function alterSchema(schema: ZodType<unknown>, newShape: ZodType<unknown>): ZodType<unknown> {
+  if (schema.description) {
+    newShape = newShape.describe(schema.description);
+  }
+  return newShape;
+}
+
 function fixSchema(schema: ZodType<unknown>): ZodType<unknown> {
   if ("unwrap" in schema && typeof schema.unwrap === "function") {
     switch (schema._zod.def.type) {
-      case "nullable": return fixSchema(schema.unwrap()).nullable();
-      case "optional": return fixSchema(schema.unwrap()).optional();
-      case "readonly": return fixSchema(schema.unwrap()).readonly();
-      case "array": return fixSchema(schema.unwrap()).array();
+      case "nullable": return alterSchema(schema, fixSchema(schema.unwrap()).nullable());
+      case "optional": return alterSchema(schema, fixSchema(schema.unwrap()).optional());
+      case "readonly": return alterSchema(schema, fixSchema(schema.unwrap()).readonly());
+      case "array": return alterSchema(schema, fixSchema(schema.unwrap()).array());
       default: throw new Error(`${schema._zod.def.type} type is not covered in fixSchema (@omer-x/next-openapi-json-generator")`);
     }
   }
   if (schema._zod.def.type === "date") {
-    return z.iso.datetime();
+    return alterSchema(schema, z.iso.datetime());
   }
   if (schema._zod.def.type === "object") {
     const { shape } = schema as ZodObject;
     const entries = Object.entries(shape);
     const alteredEntries = entries.map(([propName, prop]) => [propName, fixSchema(prop)]);
     const newShape = Object.fromEntries(alteredEntries);
-    return z.object(newShape);
+    return alterSchema(schema, z.object(newShape));
   }
   return schema;
 }
