82de1de5fc6998de68f34338b44a4a9cb0fbdfcd
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2026-07-23 09:06:54 +0700

    chore: update eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,17 @@
 import omer from "@omer-x/eslint-config";
+import plugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   ...omer,
   {
-    rules: {
+    plugins: {
+      "@typescript-eslint": plugin,
     },
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error",
+    },
+  },
+  {
+    ignores: ["dist/*"],
   },
 ];
