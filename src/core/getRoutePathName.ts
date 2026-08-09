8aeae0013fa8491ef756e93f5fa15a8872d2e5a5
Spikers <37247310+sqikerz@users.noreply.github.com>
2026-08-08 23:06:34 -0400

    feat: refactor getRoutePathName and getRouteExports for improved path handling and global scope management

--- src/core/getRoutePathName.ts
+++ src/core/getRoutePathName.ts
@@ -2,9 +2,14 @@ import path from "node:path";
 
 export default function getRoutePathName(filePath: string, rootPath: string): string {
   const dirName = path.dirname(filePath);
-  return "/" + path.relative(rootPath, dirName)
+  const relativePath = path.relative(rootPath, dirName)
     .replaceAll("[", "{")
     .replaceAll("]", "}")
-    .replaceAll("\\", "/")
-    .replaceAll(/\([^)]*\)\//g, "");
+    .replaceAll("\\", "/");
+
+  const segments = relativePath
+    .split("/")
+    .filter(segment => !(/^\([^)]*\)$/).test(segment));
+
+  return `/${segments.join("/")}`;
 }
